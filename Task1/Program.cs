c8f2dd979daf5fe5f453dbd8186d0ee7ea17469c
mkasapov <martin.kasapov.mk@gmail.com>
2018-03-07 19:02:34 +0200

    First exam task added!

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyAmount = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double sabrePrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+            int beltStudents = students - students / 6;
+            double totalMoney = 0.0;
+
+            double moneyForSabers = sabrePrice * Math.Ceiling(students + 0.1 * students);
+            double moneyForRobes = robePrice * students;
+            double moneyForBelts = beltPrice * beltStudents;
+            totalMoney = moneyForSabers + moneyForRobes + moneyForBelts;
+            if (totalMoney <= moneyAmount)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalMoney:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(totalMoney-moneyAmount):f2}lv more.");
+            }
+            
+        }
+    }
+}
